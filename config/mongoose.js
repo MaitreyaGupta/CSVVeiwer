f9ce8cc40090bb06bab0e7c71c72b3b0861e71fa
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-09-30 13:44:39 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb+srv://maitreyaguptaa:killerman@devs.syeiknh.mongodb.net/ab",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect("mongodb+srv://maitreyaguptaa:killerman@devs.syeiknh.mongodb.net/NewCSVSaver",{useNewUrlParser:true,useUnifiedTopology:true})
 const db=mongoose.connection
 
 db.once("open",function(req,res){
