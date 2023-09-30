cdfe5fbe3f971a4b445d27c56d5953272db7f5db
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-09-30 10:53:05 +0530

    Create mongoose.js

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://maitreyaguptaa:killerman@devs.syeiknh.mongodb.net/ab",{useNewUrlParser:true,useUnifiedTopology:true})
+const db=mongoose.connection
+
+db.once("open",function(req,res){
+    console.log("The database has been connected")
+})
+
+module.exports=db
